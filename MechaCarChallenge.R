83bcce645dd7c37a7cd423b84e8da83082462835
jj2773 <87623223+jj2773@users.noreply.github.com>
2021-12-19 12:10:15 -0600

    deliverable 1 upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,75 @@
+library(dplyr)
+
+# The MechaCar_mpg.csv dataset contains mpg test results for 50 prototype 
+# MechaCars. 
+
+# Multiple factors such as vehicle length, vehicle weight, spoiler angle, 
+# drivetrain, and ground clearance, were noted for each vehicle.
+
+# Using R, design a linear model that predicts the mpg 
+# of MechaCar prototypes using several variables from the MechaCar_mpg.csv file. 
+
+# first import csv file for car data
+MechaCar_df <- read.csv('MechaCar_mpg.csv',check.names=F,stringsAsFactors=F)
+
+# examine the data
+head(MechaCar_df)
+
+#output for ref
+#vehicle_length vehicle_weight spoiler_angle ground_clearance AWD      mpg
+#1       14.69710       6407.946      48.78998         14.64098   1 49.04918
+#2       12.53421       5182.081      90.00000         14.36668   1 36.76606
+#3       20.00000       8337.981      78.63232         12.25371   0 80.00000
+#4       13.42849       9419.671      55.93903         12.98936   1 18.94149
+#5       15.44998       3772.667      26.12816         15.10396   1 63.82457
+#6       14.45357       7286.595      30.58568         13.10695   0 48.54268
+
+# setup the multiple linear regression model
+lm(mpg ~ vehicle_length + 
+     vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_df)
+
+# get summary stats of model
+
+summary(lm(mpg ~ vehicle_length + 
+     vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_df))
+
+#output for reference...
+#Coefficients:
+#                   Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)      -1.040e+02  1.585e+01  -6.559 5.08e-08 ***
+#vehicle_length    6.267e+00  6.553e-01   9.563 2.60e-12 ***
+#vehicle_weight    1.245e-03  6.890e-04   1.807   0.0776 .  
+#spoiler_angle     6.877e-02  6.653e-02   1.034   0.3069    
+#ground_clearance  3.546e+00  5.412e-01   6.551 5.21e-08 ***
+#AWD              -3.411e+00  2.535e+00  -1.346   0.1852    
+---
+#Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+#
+#Residual standard error: 8.774 on 44 degrees of freedom
+#Multiple R-squared:  0.7149,	Adjusted R-squared:  0.6825 
+#F-statistic: 22.07 on 5 and 44 DF,  p-value: 5.35e-11
+
+# p values in the summary stats indicate only y intercept, vehicle_length 
+# and ground_clearance are statistically significant for our model
+
+#update the model with only those parameters
+# multiple linear regression model updated
+model <- lm(mpg ~ vehicle_length + ground_clearance,data=MechaCar_df)
+
+# get summary stats of updated model
+summary(lm(mpg ~ vehicle_length + ground_clearance,data=MechaCar_df))
+
+# get y intercept
+yval <- model$coefficients['(Intercept)']
+
+#returns -91.6
+
+# get slope coefficients
+slope_veh_len <- model$coefficients['vehicle_length']
+# returns 6.08
+slope_grd_clear <- model$coefficients['ground_clearance']
+# returns 3.57
+
+# multiple linear regression model formula becomes
+# mpg=-91.6+6.08*vehicle_length+3.57*ground_clearance
+
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # MechaCar_Statistical_Analysis
-Statistical Analysis of Car Data using R
+# Statistical Analysis of Car Data using R
+## Linear Regression to Predict MPG
+The first step in the analysis is to load the data into an R data frame using 
+the dplyr library and csv.read.
+
+![alt text](https://github.com/jj2773/MechaCar_Statistical_Analysis/blob/main/deliverable1_datahead.png)
+
+
+The summary statistics of the regression model show us that the design variables 
+that provide a non-random amount of variance to the mpg values are vehicle_length
+and ground_clearance as seen by the low Pr(<|t|) values.  
+
+
+![alt text](https://github.com/jj2773/MechaCar_Statistical_Analysis/blob/main/deliverable1_summarystats.PNG)
+
+The mpg multiple regression model thus can be represented by:
+mpg = 6.08 * vehicle_length + 3.57 * ground_clearance - 91.6
+
+where the slopes are positive for both vehicle_legnth and ground_clearance.
+
+The adjusted R-squared value indicates that 66% of the variability in mpg is represented by the model which is reasonable.
